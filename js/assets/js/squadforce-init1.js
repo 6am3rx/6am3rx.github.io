0f037fdeee7bdda88e7684e8d075f6dfa8386550
ju3tin <ju3tin@hotmail.co.uk>
2024-01-18 06:09:05 +0000

    sdf

---
+++ js/assets/js/squadforce-init1.js
@@ -0,0 +1,251 @@
+'use strict';
+
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/* eslint-disable */
+
+/******/
+(function (modules) {
+  // webpackBootstrap
+
+  /******/
+  // The module cache
+
+  /******/
+  var installedModules = {};
+  /******/
+
+  /******/
+  // The require function
+
+  /******/
+
+  function __nested_webpack_require_214__(moduleId) {
+    /******/
+
+    /******/
+    // Check if module is in cache
+
+    /******/
+    if (installedModules[moduleId]) {
+      /******/
+      return installedModules[moduleId].exports;
+      /******/
+    }
+    /******/
+    // Create a new module (and put it into the cache)
+
+    /******/
+
+
+    var module = installedModules[moduleId] = {
+      /******/
+      i: moduleId,
+
+      /******/
+      l: false,
+
+      /******/
+      exports: {}
+      /******/
+
+    };
+    /******/
+
+    /******/
+    // Execute the module function
+
+    /******/
+
+    modules[moduleId].call(module.exports, module, module.exports, __nested_webpack_require_214__);
+    /******/
+
+    /******/
+    // Flag the module as loaded
+
+    /******/
+
+    module.l = true;
+    /******/
+
+    /******/
+    // Return the exports of the module
+
+    /******/
+
+    return module.exports;
+    /******/
+  }
+  /******/
+
+  /******/
+
+  /******/
+  // expose the modules object (__webpack_modules__)
+
+  /******/
+
+
+  __nested_webpack_require_214__.m = modules;
+  /******/
+
+  /******/
+  // expose the module cache
+
+  /******/
+
+  __nested_webpack_require_214__.c = installedModules;
+  /******/
+
+  /******/
+  // define getter function for harmony exports
+
+  /******/
+
+  __nested_webpack_require_214__.d = function (exports, name, getter) {
+    /******/
+    if (!__nested_webpack_require_214__.o(exports, name)) {
+      /******/
+      Object.defineProperty(exports, name, {
+        /******/
+        configurable: false,
+
+        /******/
+        enumerable: true,
+
+        /******/
+        get: getter
+        /******/
+
+      });
+      /******/
+    }
+    /******/
+
+  };
+  /******/
+
+  /******/
+  // getDefaultExport function for compatibility with non-harmony modules
+
+  /******/
+
+
+  __nested_webpack_require_214__.n = function (module) {
+    /******/
+    var getter = module && module.__esModule ?
+    /******/
+    function getDefault() {
+      return module['default'];
+    } :
+    /******/
+    function getModuleExports() {
+      return module;
+    };
+    /******/
+
+    __nested_webpack_require_214__.d(getter, 'a', getter);
+    /******/
+
+
+    return getter;
+    /******/
+  };
+  /******/
+
+  /******/
+  // Object.prototype.hasOwnProperty.call
+
+  /******/
+
+
+  __nested_webpack_require_214__.o = function (object, property) {
+    return Object.prototype.hasOwnProperty.call(object, property);
+  };
+  /******/
+
+  /******/
+  // __webpack_public_path__
+
+  /******/
+
+
+  __nested_webpack_require_214__.p = "";
+  /******/
+
+  /******/
+  // Load entry module and return exports
+
+  /******/
+
+  return __nested_webpack_require_214__(__nested_webpack_require_214__.s = 2);
+  /******/
+}
+/************************************************************************/
+
+/******/
+)([
+  /* 0 */
+,
+/* 1 */
+
+/***/
+function (module, exports, __webpack_require__) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  /*------------------------------------------------------------------
+  
+    Theme Options
+  
+  -------------------------------------------------------------------*/
+
+  var options = {
+    scrollToAnchorSpeed: 700,
+    templates: {
+      secondaryNavbarBackItem: window.squadforceInitOptions.secondaryNavbarBackItem,
+      plainVideoIcon: '<span class="nk-video-icon"><span class="fa fa-play pl-5"></span></span>',
+      plainVideoLoadIcon: '<span class="nk-video-icon"><span class="nk-loading-icon"></span></span>',
+      audioPlainButton: '<div class="nk-audio-plain-play-pause">\n                <span class="nk-audio-plain-play">\n                    <span class="ion-play ml-3"></span>\n                </span>\n                <span class="nk-audio-plain-pause">\n                    <span class="ion-pause"></span>\n                </span>\n            </div>',
+      instagram: '<div class="col-4">\n                <a href="{{link}}" target="_blank">\n                    <img src="{{image}}" alt="{{caption}}" class="nk-img-stretch">\n                </a>\n            </div>',
+      instagramLoadingText: 'Loading...',
+      instagramFailText: 'Failed to fetch data',
+      instagramApiPath: 'php/instagram/instagram.php',
+      twitter: '<div class="nk-twitter">\n                <span class="nk-twitter-icon fa fa-twitter"></span>\n                <div class="nk-twitter-name">\n                      <a href="https://twitter.com/{{screen_name}}" target="_blank">@{{screen_name}}</a>\n                </div>\n                <div class="nk-twitter-date">\n                    <span>{{date}}</span>\n                </div>\n                <div class="nk-twitter-text">\n                   {{tweet}}\n                </div>\n            </div>',
+      twitterLoadingText: 'Loading...',
+      twitterFailText: 'Failed to fetch data',
+      twitterApiPath: 'php/twitter/tweet.php',
+      countdown: '<div class="nk-hexagon">\n                <div class="nk-hexagon-inner"></div>\n                <span>%D</span>\n                <small>Days</small>\n            </div>\n            <div class="nk-hexagon">\n                <div class="nk-hexagon-inner"></div>\n                <span>%H</span>\n                <small>Hours</small>\n            </div>\n            <div class="nk-hexagon">\n                <div class="nk-hexagon-inner"></div>\n                <span>%M</span>\n                <small>Minutes</small>\n            </div>\n            <div class="nk-hexagon">\n                <div class="nk-hexagon-inner"></div>\n                <span>%S</span>\n                <small>Seconds</small>\n            </div>'
+    }
+  };
+  exports.options = options;
+  /***/
+},
+/* 2 */
+
+/***/
+function (module, exports, __nested_webpack_require_5725__) {
+  module.exports = __nested_webpack_require_5725__(3);
+  /***/
+},
+/* 3 */
+
+/***/
+function (module, exports, __nested_webpack_require_5844__) {
+  "use strict";
+
+  var _options = __nested_webpack_require_5844__(1);
+
+  if (typeof window.SquadForce !== 'undefined') {
+    window.SquadForce.setOptions(_options.options);
+    window.SquadForce.init();
+  }
+  /***/
+
+}
+/******/
+]);
+/******/ })()
+;
